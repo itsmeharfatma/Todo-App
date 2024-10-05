37b66fb2a8be75db53b5179ebf566cb75ba018a5
Mehar Fatma <mehar.fatma202@gmail.com>
2024-10-05 21:26:24 +0530

    initial commit with base setup and structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const slicedData = data.slice(0, 10);
+
+  const fetchTodoData = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((todoData) => setData(todoData))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchTodoData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="px-10 py-6 space-y-4">
+      <h1 className="text-2xl font-semibold">TODOs:</h1>
+      <ol className="list-decimal list-inside space-y-3">
+        {slicedData.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}{" "}
+            <span
+              className={`${todo.completed ? "text-green-600" : "text-red-600"} font-semibold`}
+            >
+              {todo.completed ? "Done" : "Pending"}
+            </span>
+          </li>
+        ))}
+      </ol>
+    </section>
   );
-}
+};
 
 export default App;
